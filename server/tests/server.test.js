b3fb5b538a134010f7b3f668c46d4e0206485e1c
Chanyee Kim <ck@Chanyees-MacBook-Pro.local>
2018-08-11 19:00:31 -0300

    update test template

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,11 +1,22 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Company} = require('./../models/company');
 
+const companies = [{
+  _id: new ObjectID(),
+  name: 'First test company'
+}, {
+  _id: new ObjectID(),
+  name: 'Second test company'
+}];
+
 beforeEach((done) => {
-  Company.remove({}).then(() => done());
+  Company.remove({}).then(() => {
+    return Company.insertMany(companies);
+  }).then(() => done());
 });
 
 describe('POST /companies', () => {
@@ -24,7 +35,7 @@ describe('POST /companies', () => {
           return done(err);
         }
 
-        Company.find().then((companies) => {
+        Company.find({name}).then((companies) => {
           expect(companies.length).toBe(1);
           expect(companies[0].name).toBe(name);
           done();
@@ -43,9 +54,48 @@ describe('POST /companies', () => {
         }
 
         Company.find().then((companies) => {
-          expect(companies.length).toBe(0);
+          expect(companies.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+describe('GET /companies', () => {
+  it('should get all companies', (done) => {
+    request(app)
+      .get('/companies')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.companies.length).toBe(2);
+      })
+      .end(done);
+  });
+});
+
+describe('GET /companies/:id', () => {
+  it('should return company doc', (done) => {
+    request(app)
+      .get(`/companies/${companies[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.company.name).toBe(companies[0].name);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+    request(app)
+      .get(`/companies/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    request(app)
+      .get(`/companies/123`)
+      .expect(404)
+      .end(done);
   });
 });
